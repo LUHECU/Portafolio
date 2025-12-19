8cb6472a863e44098f072f2a70d1aac4ba70a9a4
Luis Hernandez <97182540+LUHECU@users.noreply.github.com>
2025-12-18 18:56:33 -0600

    Project section added

---
+++ src/data/projects.ts
@@ -0,0 +1,42 @@
+export const PROJECTS_EN = [
+    {
+        "id": "1",
+        "title":"Simple-Calc",
+        "description":"Small website made for a former coworker to safe time during a billing process. It retrives the original value from an amount that already includes a 13% VAT.",
+        "stack":["Astro", "JavaScript", "CSS"],
+        "image":"/images/calculo-simple-1.webp",
+        "status":"Completed",
+        "url":"https://calculo-simple.vercel.app/"
+    },
+    {
+        "id": "2",
+        "title":"Simple-Calc",
+        "description":"Small website made for a former coworker to safe time during a billing process. It retrives the original value from an amount that already includes a 13% VAT.",
+        "stack":["Astro", "JavaScript", "CSS"],
+        "image":"/images/calculo-simple-1.webp",
+        "status":"Completed",
+        "url":"https://calculo-simple.vercel.app/"
+    },
+]
+
+export const PROJECTS_ES = [
+    {
+        "id": "1",
+        "title":"Calculo-Simple",
+        "description":"Página web sencilla hecha para ayudar a un excompañero de trabajo en un proceso de facturación. Obtiene el valor inicial de un monto con 13% de IVA.",
+        "stack":["Astro", "JavaScript", "CSS"],
+        "image":"/images/calculo-simple-1.webp",
+        "status":"Terminado",
+        "url":"https://calculo-simple.vercel.app/"
+    },
+    {
+        "id": "2",
+        "title":"Calculo-Simple",
+        "description":"Página web sencilla hecha para ayudar a un excompañero de trabajo en un proceso de facturación. Obtiene el valor inicial de un monto con 13% de IVA.",
+        "stack":["Astro", "JavaScript", "CSS"],
+        "image":"/images/calculo-simple-1.webp",
+        "status":"Terminado",
+        "url":"https://calculo-simple.vercel.app/"
+    },
+
+]
